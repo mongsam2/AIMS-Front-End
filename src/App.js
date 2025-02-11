b63edaa3606cc35b4684746e311c4d2dcbfc0113
jxxhyo <lcyk0123@sju.ac.kr>
2025-02-11 17:24:08 +0900

    [feat] #97 - 화면 크기 조절 가능 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 
 import MainPage from "./pages/MainPage";
 import StudentRecord from "./pages/StudentRecord";
@@ -8,18 +9,27 @@ import EssayTest from "./pages/EssayTest";
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
 
+const FixedContainer = styled.div`
+  width: auto;
+  height: auto;
+  margin: 0 auto;
+  overflow: auto;
+`;
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="" element={<Login />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/document-review" element={<DocumentReview />} />
-        <Route path="/student-record" element={<StudentRecord />} />
-        <Route path="/essay-test" element={<EssayTest />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </Router>
+    <FixedContainer>
+      <Router>
+        <Routes>
+          <Route path="" element={<Login />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/document-review" element={<DocumentReview />} />
+          <Route path="/student-record" element={<StudentRecord />} />
+          <Route path="/essay-test" element={<EssayTest />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </Router>
+    </FixedContainer>
   );
 }
 
