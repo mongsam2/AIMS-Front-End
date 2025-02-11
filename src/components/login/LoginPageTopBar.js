4dbac25bb7b0e10f4ed5eb2eb92546df96f41084
jxxhyo <lcyk0123@sju.ac.kr>
2025-02-11 13:25:25 +0900

    [feat] #80 - 로그인 페이지 API 연동 및 디자인 수정

--- src/components/login/LoginPageTopBar.js
+++ src/components/login/LoginPageTopBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
+import logoicon from "../../assets/aims_logo.png";
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
@@ -14,9 +15,15 @@ const LoginPageContainerTop = styled.div`
   width: 96%;
   height: 10%;
   display: flex;
-  justify-content: space-between;
+  justify-content: left;
   align-items: center;
   margin-bottom: 1%;
+  gap: 0.5rem;
+`;
+
+const LoginPageContainerLogoImg = styled.img`
+  width: 3.4rem;
+  height: 2.3rem;
 `;
 
 const LoginPageContainerLogo = styled.p`
